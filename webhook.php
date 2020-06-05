934e83ae8e221863148eee29d7dc76ab6f06562f
Pablo Varela <pablops3@gmail.com>
2020-06-04 23:09:35 -0300

    Completado 0.3b

--- webhook.php
+++ webhook.php
@@ -6,12 +6,14 @@ require('vendor/autoload.php');
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
 
+$rawdata = file_get_contents("php://input");
+
 file_put_contents("webhook.json","/---------------------------------------------------\\\n",FILE_APPEND);
 file_put_contents("webhook.json","Llamada:\n",FILE_APPEND);
 file_put_contents("webhook.json","GET:\n",FILE_APPEND);
 file_put_contents("webhook.json",json_encode($_GET) . "\n",FILE_APPEND);
 file_put_contents("webhook.json","POST:\n",FILE_APPEND);
-file_put_contents("webhook.json",json_encode($_POST) . "\n",FILE_APPEND);
+file_put_contents("webhook.json",json_encode($rawdata) . "\n",FILE_APPEND);
 file_put_contents("webhook.json","---- Resultado ----\n",FILE_APPEND);
 
 if(isset($_GET['topic'])){
