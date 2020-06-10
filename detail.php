7902f5f0434c583d3d6fea53b6278fd0c997ed99
Pablo Varela <pablops3@gmail.com>
2020-06-10 19:49:20 -0300

    Failover para detail sin $_POST

--- detail.php
+++ detail.php
@@ -1,6 +1,13 @@
 <?php 
 
+if(!isset($_POST['title']) || !isset($_POST['price'])){
+    header('index.php');
+    return false;
+}
+
 require __DIR__ .  '/vendor/autoload.php';
+
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 $preference = new MercadoPago\Preference();
 
